c27fa6683a0638ac3d07fc05090320c8803a4621
Victor Wanderzon <70594305+vwfuentes@users.noreply.github.com>
2020-09-19 19:05:07 -0400

    Add files via upload
    HomeWork Jatun

--- ProyectoSemaforo"PracticaUnidad 5: if-else"/Semaforo.java
+++ ProyectoSemaforo"PracticaUnidad 5: if-else"/Semaforo.java
@@ -1,11 +1,11 @@
 public class Semaforo{
-        private int location;
+    private int location;
     private int colorRojo=1;
     private int colorAmarillo=2;
     private int colorVerde=0;
     public String cambiarDeColor(int numero){
         String res = new String("color rojo");
-        
+
         int residuo=numero%3;
         if(residuo == colorRojo){
             res = new String("colorRojo");
